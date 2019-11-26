a0cb71597a740879149f96b0d3376f0a04555d2e
Victoria Li <VLi@lcs.on.ca>
2019-11-26 10:44:05 -0500

    Initial Commit

---
+++ English or French/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  English or French
+//
+//  Created by Li, Muchen on 2019-11-26.
+//  Copyright © 2019 Li, Muchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
